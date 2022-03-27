4ba34c27466f1d805693828c0d0ffd36d852982f
Brett Kearns <brettc.kearns@gmail.com>
2022-03-27 18:50:24 -0400

    add components, css styling

---
+++ src/features/main/Similarity.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Similarity() {
+  return (
+    <div>
+      <h2>
+        Today is puzzle number n. The nearest word has a similarity of n, the
+        tenth-nearest has a similarity of n and the one thousandth nearest word
+        has a similarity of n.
+      </h2>
+    </div>
+  );
+}
