50a2a655ed559fe125368ec9c3b337c3b364a830
Brett Kearns <brettc.kearns@gmail.com>
2022-03-28 16:49:40 -0400

    fix react script version comp, change to html tag

--- src/features/main/Similarity.js
+++ src/features/main/Similarity.js
@@ -3,11 +3,11 @@ import React from "react";
 export function Similarity() {
   return (
     <div>
-      <h2>
+      <p>
         Today is puzzle number n. The nearest word has a similarity of n, the
         tenth-nearest has a similarity of n and the one thousandth nearest word
         has a similarity of n.
-      </h2>
+      </p>
     </div>
   );
 }
