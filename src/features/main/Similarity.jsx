37a38dc4e948e88cc820e4107625a631f8f495cd
B.Kearns <95045381+BrettKearns@users.noreply.github.com>
2022-03-29 20:01:44 -0400

    add components (#1)
    * add components, css styling
    
    * fix react script version comp, change to html tag
    
    * file type change
    
    * index fix
    
    * delete old files

---
+++ src/features/main/Similarity.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Similarity() {
+  return (
+    <>
+      <p>
+        Today is puzzle number n. The nearest word has a similarity of n, the
+        tenth-nearest has a similarity of n and the one thousandth nearest word
+        has a similarity of n.
+      </p>
+    </>
+  );
+}
